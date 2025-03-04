e99ec8fc850242195a0703e901239cc603928e6d
mdrt1395 <marcodavidrt95@gmail.com>
2025-03-03 20:09:33 -0600

    finished basic crud for basic app using entity framework

---
+++ WebAPIApplicationLol/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIApplicationLol.Models.Entities;
+
+namespace WebAPIApplicationLol.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
